d01fe13c8419434c1290e88fb592ee7da488721b
lifewinning <lifewinning@gmail.com>
2014-10-25 22:41:22 -0400

    all these things!

--- src/script.js
+++ src/script.js
@@ -3,14 +3,14 @@ var toner = new L.StamenTileLayer("toner-lite")
 map.addLayer(toner);
 map.locate({setView: true, minZoom: 20, maxZoom: 22});
 
+var thisIcon = L.MakiMarkers.icon({icon:'star-stroked', color: "#feb24c", size: "s"});
 //you are here
 function onLocationFound(e) {
+
+	var youAreHere = L.MakiMarkers.icon({color: "#2FBD57", size: "s"})
     var radius = e.accuracy / 2;
 
-    L.marker(e.latlng).addTo(map)
-        .bindPopup("You are within " + radius + " meters from this point");
-
-    //L.circle(e.latlng, radius).addTo(map);
+    L.marker(e.latlng, {icon: youAreHere}).addTo(map).bindPopup("You are approximately here");
 }
 
 map.on('locationfound', onLocationFound);
@@ -19,8 +19,9 @@ map.on('locationfound', onLocationFound);
 var things = [];
 
 var drawnItems = new L.FeatureGroup();
+var hellaCheat = new L.FeatureGroup();
+map.addLayer(hellaCheat);
 map.addLayer(drawnItems);
-
 // Set the title to show on the polygon button
 L.drawLocal.draw.toolbar.buttons.polygon = 'Polygons!';
 
@@ -46,40 +47,66 @@ draw: {
 			color: '#feb24c'
 		}
 	},
-	marker: true
+	circle: false,
+	rectangle: false,
+	marker: {icon: thisIcon}
 },
-edit: {
-	featureGroup: drawnItems,
-	remove: true
-}
+// NO EDITING IN THIS BECAUSE I MADE AWFUL CHOICES I AM SORRY FOR WHAT I HAVE DONE
+// edit: {
+// 	featureGroup: drawnItems,
+// 	remove: true
+// }
 });
 map.addControl(drawControl);
 
+
 map.on('draw:created', function (e) {
 	var layer = e.layer;
-	var layers = e.layers;
-	layerID = Math.floor(Math.random()*666);
-	//drawnItems.addLayer(layer);
-	//push new layer to array
-	var geojson = layer.toGeoJSON();
-	drawnItems.addLayer(geojson);
-	geojson.properties["id"] = layerID;
-	geojson.properties["notes"] = 
-	stringy = JSON.stringify(geojson);
-	things.push(stringy);
-	//console.log(things);
+	var layerType = e.layerType;
+	var popupContent = '<div><h4>Item</h4><input id="type" name="type" value=""><h4>Notes</h4><textarea id="notes" name="notes" value=""></textarea><button id="add">Save</button></div>';
+	
+	//NO ONE SHOULD EVER WRITE CODE LIKE THIS IT IS HACKY AND THE WORST BUT IT DEMONSTRATES AN EXAMPLE ADEQUATELY
+	hellaCheat.addLayer(layer).bindPopup(popupContent).openPopup();
+	
+	//save layer as geojson object by adding data to field
+	if (layer = layer){
+		L.DomUtil.get('add').onclick = function(){
+		//layer is a geojson
+			var geojson = layer.toGeoJSON();
+		//layer needs to be re-styled when added back to the map	
+			var style = {color: '#feb24c'};
+		//add properties to geojson from popup
+			geojson.properties["type"] = L.DomUtil.get("type").value;
+			geojson.properties["notes"] = L.DomUtil.get("notes").value;
+			//NEVER DO THIS EITHER THIS IS AWFUL
+			var thisPopupContent = '<div><h4>Item</h4><input id="type" name="type" value='+JSON.stringify(geojson.properties["type"])+'>'+
+			'<h4>Notes</h4><textarea id="notes" name="notes">'+geojson.properties["notes"]+
+			'</textarea><button id="edit">Save</button></div>';
+		//add geojson with stored properties to the map, removing the original layer 
+			if (layerType !== 'marker'){
+				L.geoJson(geojson, {style: style}).addTo(drawnItems).bindPopup(thisPopupContent);
+				hellaCheat.clearLayers();
+				console.log(drawnItems);
+			} else {
+				L.geoJson(geojson, { pointToLayer: function (feature, latlng) {
+					return L.marker(latlng, {icon: thisIcon});
+				}
+				}).addTo(drawnItems).bindPopup(thisPopupContent);
+				hellaCheat.clearLayers();		
+			}
+			stringy = JSON.stringify(geojson);
+			things.push(stringy);
+			console.log("geojson" + things);
+		}
+	}
 });
-map.on('draw:edited', function(e){
-	var layer = e.layer;
+map.on('draw:edited', function (e) {
 	var layers = e.layers;
-	var things = [];
-	layers.eachLayer(function(){
-	var geojson = layer.toGeoJSON();
-		geojson.properties["id"] = layerID;
-		stringy = JSON.stringify(geojson);
-		things.push(stringy);
+	var countOfEditedLayers = 0;
+	layers.eachLayer(function(layer) {
+		countOfEditedLayers++;
 	});
-
+	console.log("Edited " + countOfEditedLayers + " layers");
 });
 
 
